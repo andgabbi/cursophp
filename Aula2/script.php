c014a9459a15ca7bfc8fcbcfa4403b7b8d583186
Gabriele Andrade <gabbimdeandrade@gmail.com>
2021-10-18 18:48:20 -0300

    [New] Form

---
+++ Aula2/script.php
@@ -0,0 +1,32 @@
+<?php
+
+    $nome = $_POST['nome'];
+    $idade = $_POST['idade'];
+
+
+    $categorias = ['infantil', 'adolescente', 'adulto'];
+
+
+    if($idade >= 6 && $idade <= 12) {
+        for ($i = 0; $i < count($categorias); $i++) {
+            if($categorias[$i] == 'infantil') {
+                echo('O nadador ' . $nome .  ' pertence a categoria ' . $categorias[$i]);
+            }
+        }
+    }
+    else if ($idade >= 13 && $idade < 18) {
+        for ($i = 0; $i < count($categorias); $i++) {
+            if($categorias[$i] == 'adolescente') {
+                echo('O nadador ' . $nome .  ' pertence a categoria ' . $categorias[$i]);
+            }
+        }
+    }
+    else {  
+        for ($i = 0; $i < count($categorias); $i++) {
+            if($categorias[$i] == 'adulto') {
+                echo('O nadador ' . $nome .  ' pertence a categoria ' . $categorias[$i]);
+            }
+        }
+    }
+
+?>
